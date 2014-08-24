a66c1ffe8162d2a3e2c61bdac79339a1d371e6aa
Ryanm14 <ryanm1114@gmail.com>
2014-08-24 17:46:24 -0400

    Added ads , testing back buttons and animations

--- AQuietNight/src/main/java/com/milesstudios/aquietnight/main.java
+++ AQuietNight/src/main/java/com/milesstudios/aquietnight/main.java
@@ -4,18 +4,18 @@ package com.milesstudios.aquietnight;
  * Created by Ryanm14 on 7/14/2014.
  */
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.app.Activity;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 public class main extends Activity {
-    int wood_counter, berries_counter;
-    Button fix_walls, buildings, trading, crafting, quests, wood, trap, leaves;
+    Button fix_walls, buildings, trading, crafting, quests;
     TextView log, storage;
 
     @Override
@@ -23,63 +23,39 @@ public class main extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cave);
-        wood_counter = 0;
-        berries_counter = 0;
+        buildings = (Button) findViewById(R.id.buildings);
         log = (TextView) findViewById(R.id.log);
         storage = (TextView) findViewById(R.id.storage);
-        wood = (Button) findViewById(R.id.wood);
-        leaves = (Button) findViewById(R.id.leaves);
-        final Animation animAlpha = AnimationUtils.loadAnimation(this, R.anim.anim_alpha);
         log.setTextSize(25);
         storage.setWidth(50);
         log.setText("THIS APP IS FOR TESTING HI!");
+        // Look up the AdView as a resource and load a request.
+        AdView adView = (AdView) this.findViewById(R.id.ad);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
         //TODO Check other buttons and scaling
 
-
-
-        wood.setOnClickListener(new View.OnClickListener() {
+        buildings.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View v) {
-                wood_counter += 1;
-                storage.append("Wood:" + wood_counter);
-                storage.setLines(20);
-                // set the color red first.
-                wood.setEnabled(false);
-                wood.startAnimation(animAlpha);
-
-                // change to original after 5 secs.
-                new Handler().postDelayed(new Runnable() {
-
-                    public void run() {
-                        //wood.setBackgroundColor(Color.WHITE);
-                        wood.setEnabled(true);
-                    }
-                }, 6000);
+                Intent openForest = new Intent(main.this, Forest.class);
+                startActivity(openForest);
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_left);
 
             };
 
         });
 
-        leaves.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
-                berries_counter += 1;
-                storage.append("Leaves:" + berries_counter);
-                // set the color red first.
-                leaves.setEnabled(false);
-                leaves.startAnimation(animAlpha);
 
-                // change to original after 5 secs.
-                new Handler().postDelayed(new Runnable() {
 
-                    public void run() {
-                        //wood.setBackgroundColor(Color.WHITE);
-                        leaves.setEnabled(true);
-                    }
-                }, 8000);
 
-            };
 
-        });
+
     }
+    @Override
+    public void onBackPressed() {
+        // TODO Auto-generated method stub
+        super.onBackPressed();
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
+    }
+
 }
-
-
